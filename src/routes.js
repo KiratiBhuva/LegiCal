8993b86318fc5e57d2c010443e04422dc62d29d1
KiratiBhuva <35611972+KiratiBhuva@users.noreply.github.com>
2019-11-06 20:02:35 -0800

    first commit

---
+++ src/routes.js
@@ -0,0 +1,81 @@
+/*!
+
+=========================================================
+* Paper Dashboard React - v1.1.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/paper-dashboard-react
+* Copyright 2019 Creative Tim (https://www.creative-tim.com)
+
+* Licensed under MIT (https://github.com/creativetimofficial/paper-dashboard-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import Dashboard from "views/Dashboard.jsx";
+import Notifications from "views/Notifications.jsx";
+import Icons from "views/Icons.jsx";
+import Typography from "views/Typography.jsx";
+import TableList from "views/Tables.jsx";
+import Maps from "views/Map.jsx";
+import UserPage from "views/User.jsx";
+import UpgradeToPro from "views/Upgrade.jsx";
+
+var routes = [
+  {
+    path: "/dashboard",
+    name: "Dashboard",
+    icon: "nc-icon nc-bank",
+    component: Dashboard,
+    layout: "/admin"
+  },
+  {
+    path: "/bills",
+    name: "Bills",
+    icon: "nc-icon nc-paper",
+    component: TableList,
+    layout: "/admin"
+  },
+  {
+    path: "/billtracking",
+    name: "Bill Tracking",
+    icon: "nc-icon nc-sound-wave",
+    component: Icons,
+    layout: "/admin"
+  },
+  {
+    path: "/preferences",
+    name: "My Preferences",
+    icon: "nc-icon nc-settings",
+    component: Icons,
+    layout: "/admin"
+  },
+  {
+    path: "/search",
+    name: "Search",
+    icon: "nc-icon nc-zoom-split",
+    component: Notifications,
+    layout: "/admin"
+  },
+  {
+    path: "/senator-page",
+    name: "Senator Profile",
+    icon: "nc-icon nc-single-02",
+    component: UserPage,
+    layout: "/admin"
+  },
+  
+  {
+    pro: true,
+    path: "/upgrade",
+    name: "Upgrade to PRO",
+    icon: "nc-icon nc-spaceship",
+    component: UpgradeToPro,
+    layout: "/admin"
+  }
+];
+export default routes;
